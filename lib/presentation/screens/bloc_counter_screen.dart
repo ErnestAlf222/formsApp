9fedd68b2dd01f6ad35a93c5cbe0e1ad1548fc7a
Ernesto Alfonso Sanchez Montiel <ernest.alf45@gmail.com>
2024-05-29 13:49:14 -0600

    bloc, counter screen pantalla basica

---
+++ lib/presentation/screens/bloc_counter_screen.dart
@@ -0,0 +1,61 @@
+
+import 'package:flutter/material.dart';
+
+class BlocCounterScreen extends StatelessWidget {
+  const BlocCounterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    
+    final textStyles = Theme.of(context).textTheme;
+
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('BlocCounterScreen'),
+        actions: [
+
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.refresh_rounded,
+            ),
+          ),
+        ],
+      ),
+
+      body: const Center(
+        child: Text('Counter value: xxxx'),
+      ),
+
+      
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+        
+            FloatingActionButton(
+              heroTag: '1',
+              onPressed: () {},
+              child:  Text('+3', style: textStyles.titleLarge),
+            ),
+            const SizedBox(height: 15),
+            FloatingActionButton(
+              heroTag: '2',
+              onPressed: () {},
+              child:  Text('+2', style: textStyles.titleLarge),
+            ),
+            const SizedBox(height: 15),
+            FloatingActionButton(
+              heroTag: '3',
+              onPressed: () {},
+              child:  Text('+1', style: textStyles.titleLarge),
+            ),
+        
+          ],
+        ),
+      ),
+    );
+  }
+}
