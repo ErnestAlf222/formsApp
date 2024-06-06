4752fd5f24803c8a9979b0c2d606545d627aa1e1
Ernesto Alfonso Sanchez Montiel <ernest.alf45@gmail.com>
2024-06-06 14:18:28 -0600

    validaciones en el formulario

--- lib/presentation/screens/register_screen.dart
+++ lib/presentation/screens/register_screen.dart
@@ -45,28 +45,82 @@ class _RegisterView extends StatelessWidget {
   }
 }
 
-class _RegisterForm extends StatelessWidget {
+class _RegisterForm extends StatefulWidget {
   const _RegisterForm();
+
+  @override
+  State<_RegisterForm> createState() => _RegisterFormState();
+}
+
+class _RegisterFormState extends State<_RegisterForm> {
+  // Creación de clave global
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  String username = '';
+  String email = '';
+  String password = '';
 
   @override
   Widget build(BuildContext context) {
     return Form(
+        key: _formKey,
         child: Column(
-      children: [
-        // 1. TextFormfield
-        const CustomTextFormField(),
-        const SizedBox(height: 10),
-        const CustomTextFormField(),
-        const SizedBox(height: 10),
-        const CustomTextFormField(),
+          children: [
+            // 1. TextFormfield
+            CustomTextFormField(
+              icon: Icons.unsubscribe_rounded,
+              label: 'Nombre de usuario',
+              onChanged: (value) => username = value,
+              validator: (value) {
+                if (value == null || value.isEmpty) return 'Campo requerido';
+                if (value.trim().isEmpty) return 'Campo requerido';
+                if (value.length < 6) return 'Más de 6 letras';
+                return null;
+              },
+            ),
+            const SizedBox(height: 30),
 
-        const SizedBox(height: 20),
-        FilledButton.tonalIcon(
-          onPressed: () {},
-          icon: const Icon(Icons.save),
-          label: const Text('Guardar'),
-        ),
-      ],
-    ));
+            CustomTextFormField(
+              icon: Icons.alternate_email_outlined,
+              label: 'Correo',
+              onChanged: (value) => email = value,
+              validator: (value) {
+                if (value == null || value.isEmpty) return 'Campo requerido';
+                if (value.trim().isEmpty) return 'Campo requerido';
+
+                final emailRegExp = RegExp(
+                  r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$',
+                );
+                if (!emailRegExp.hasMatch(value)) return 'No tiene formato de correo';
+                return null;
+              },
+            ),
+            const SizedBox(height: 30),
+
+            CustomTextFormField(
+              icon: Icons.password_rounded,
+              label: 'Contraseña',
+              obscureText: true,
+              onChanged: (value) => password = value,
+               validator: (value) {
+                if (value == null || value.isEmpty) return 'Campo requerido';
+                if (value.trim().isEmpty) return 'Campo requerido';
+                if (value.length < 6) return 'Más de 6 letras';
+                return null;
+              },
+            ),
+            const SizedBox(height: 45),
+
+            FilledButton.tonalIcon(
+              icon: const Icon(Icons.save),
+              label: const Text('Guardar'),
+              onPressed: () {
+                // currentState => valor actual
+                final isValid = _formKey.currentState!.validate();
+                if (!isValid) return;
+                // print('$username, $email, $password');
+              },
+            ),
+          ],
+        ));
   }
 }
--- lib/presentation/widgets/inputs/custom_text_form_field.dart
+++ lib/presentation/widgets/inputs/custom_text_form_field.dart
@@ -1,8 +1,24 @@
-
 import 'package:flutter/material.dart';
 
 class CustomTextFormField extends StatelessWidget {
-  const CustomTextFormField({super.key});
+  final IconData icon;
+  final String? label;
+  final String? hint;
+  final String? errorMessage;
+  final Function(String)? onChanged;
+  final String? Function(String?)? validator;
+  final bool? obscureText; 
+
+  const CustomTextFormField({
+    super.key,
+    required this.icon,
+    this.label,
+    this.hint,
+    this.errorMessage,
+    this.onChanged,
+    this.validator, 
+    this.obscureText = false,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -10,23 +26,31 @@ class CustomTextFormField extends StatelessWidget {
 
     final border = OutlineInputBorder(
       borderRadius: BorderRadius.circular(40),
-
-
     );
     return TextFormField(
-      onChanged: (value) {
-        
-      },
-      validator: (value) {
-        if (value == null) return 'Campo es requerido';
-        if(value.isEmpty) return 'Campo es requerido';
-        if (value.trim().isEmpty) return 'Campo es requerido';
-        return null;
-      },
+      onChanged: onChanged,
+      validator: validator,
+      //? obscureText => oculta la contraseña
+      obscureText: obscureText!,
+
       decoration: InputDecoration(
-        enabledBorder: border,
-        focusedBorder: border.copyWith( borderSide: BorderSide(color: colors.primary))
-      ),
+          enabledBorder: border,
+          focusedBorder:
+              border.copyWith(borderSide: BorderSide(color: colors.primary)),
+          errorBorder: border.copyWith(
+              borderSide: BorderSide(color: Colors.red.shade800)),
+          focusedErrorBorder: border.copyWith(
+              borderSide: BorderSide(color: Colors.red.shade800)),
+          
+          isDense: true,
+          
+          label: label != null ? Text(label!) : null,
+          hintText: hint,
+          focusColor: colors.primary,
+          suffixIcon: Icon(icon, color: colors.primary, size: 25),
+          errorText: errorMessage,
+          
+          ),
     );
   }
 }
